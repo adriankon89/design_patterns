24701c6e7f1f480cdd7fbc3b1d4040a4b2396d35
Adrian <adriankon89@gmail.com>
2024-01-11 15:29:16 +0100

    Adapter

---
+++ adapter/Main.php
@@ -0,0 +1,49 @@
+<?php
+
+interface Player
+{
+    public function play();
+}
+
+class SimplePlayer implements Player
+{
+    public function __construct(private string $videoUrl) {}
+
+    public function play()
+    {
+        echo 'simple player';
+    }
+}
+
+class OrangeSitePlayer
+{
+    public function __construct(private string $videoUrl) {}
+
+    public function play()
+    {
+        echo 'organge site player';
+    }
+
+    public function userValid() {}
+
+    public function verifyUserAge() {}
+}
+
+class OrangePlayerAdapter implements Player
+{
+    public function __construct(private OrangeSitePlayer $player) {}
+
+    public function play()
+    {
+        if ($this->player->userValid() && $this->player->verifyUserAge()) {
+            $this->player->play();
+        }
+    }
+}
+
+$simplePlayer = new SimplePlayer('url');
+$simplePlayer->play();
+echo "\n";
+$orangePlayer = new OrangeSitePlayer('url');
+$orangePlayerAdapter = new OrangePlayerAdapter($orangePlayer);
+$orangePlayerAdapter->play();
